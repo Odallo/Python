5ec5e5da7fa26d64d95757014c9f52b5a05d50d0
Odallo <eugeneodali181@gmail.com>
2025-02-19 15:40:20 +0300

    More python

--- cisco.py
+++ cisco.py
@@ -16,11 +16,24 @@
 
 #print("largestNumber =", largestNumber)
 
-#conditional execution
-text = input("Please enter a name: ")
-if text == "Alice":
-    print("Welcome Alice")
-elif text == "Bob":
-    print("Welcome Bob")
+# conditional execution
+# text = input("Please enter a name: ")
+# if text == "Alice":
+#     print("Welcome Alice")
+# elif text == "Bob":
+#     print("Welcome Bob")
+# else:
+#     print("You are not Alice or Bob")
+
+
+# tax scenario
+income = float(input("Enter your income: "))
+if income < 85528:
+	tax = income * 0.18 - 556.02
 else:
-    print("You are not Alice or Bob")
+    tax = 14839.02 + (income/85528) * 0.32 
+
+tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
+
+
