372efe8e8dafd77447a8523f45b736017fec832c
Odallo <eugeneodali181@gmail.com>
2025-02-19 15:26:54 +0300

    More python

--- cisco.py
+++ cisco.py
@@ -1,16 +1,26 @@
 #n = int(input("Enter your number: "))
 #print(n>=100)
 
-first = int(input("Enter your number: "))
-second = int(input("Enter your number: "))
-third = int(input("Enter your number: "))
+##
+# first = int(input("Enter your number: "))
+#second = int(input("Enter your number: "))
+#third = int(input("Enter your number: "))
 
-largestNumber = first
+#largestNumber = first
 
-if second > largestNumber:
-    largestNumber = second
+#if second > largestNumber:
+    #largestNumber = second
 
-elif third > largestNumber:
-    largestNumber = third
+#elif third > largestNumber:
+    #largestNumber = third
 
-print("largestNumber =", largestNumber)
+#print("largestNumber =", largestNumber)
+
+#conditional execution
+text = input("Please enter a name: ")
+if text == "Alice":
+    print("Welcome Alice")
+elif text == "Bob":
+    print("Welcome Bob")
+else:
+    print("You are not Alice or Bob")
