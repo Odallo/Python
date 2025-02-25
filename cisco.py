0cd7f224931486c3122fae80fa6b59172b4ff13c
Odallo <eugeneodali181@gmail.com>
2025-02-25 13:48:56 +0300

    More python

--- cisco.py
+++ cisco.py
@@ -105,3 +105,15 @@
 #         continue
 #     word_without_vowel += i
 # print(word_without_vowel)
+
+x = 4
+y = 1
+ 
+a = x & y
+b = x | y
+c = ~x  # tricky!
+d = x ^ 5
+e = x >> 2
+f = x << 2
+ 
+print(a, b, c, d, e, f)
