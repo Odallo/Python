46ef19342fa26e99e0f47c62a059e4b810fd2a02
Odallo <eugeneodali181@gmail.com>
2025-02-26 15:15:07 +0300

    Auto-commit

--- toPdf.py
+++ toPdf.py
@@ -1,1 +1,10 @@
-import 
+import fitz
+from docx import Document
+def pdf_to_word(pdf_path, word_path):
+    pdf_document = fitz.open(pdf_path)
+    doc = Document()
+    for page_number in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_number)
+        text = page.get_text()
+        doc.add_paragraph(text)
+    doc.save(word_path)
