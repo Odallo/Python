a464ff8eb779e451ab6930b7703f1dd2b2dc1486
Odallo <eugeneodali181@gmail.com>
2025-03-01 14:13:15 +0300

    Auto-commit

--- cisco.py
+++ cisco.py
@@ -152,23 +152,56 @@
 #     return "Gutene Morgen"
 # print(greetings())
 
-def is_year_leap(year):
-    if year % 4 != 0:
-        return False
-    elif year % 100 != 0:
-        return True
-    elif year % 400 != 0:
-        return False
-    else:
-        return True
+# def is_year_leap(year):
+#     if year % 4 != 0:
+#         return False
+#     elif year % 100 != 0:
+#         return True
+#     elif year % 400 != 0:
+#         return False
+#     else:
+#         return True
    
 
-test_data = [1900, 2001, 2016, 1987]
-test_results = [False, True, True, False]
-for i in range(len(test_data)):
-    yr = test_data[i]
-    print(yr,"->",end="")
-    result = is_year_leap(yr)
+# test_data = [1900, 2001, 2016, 1987]
+# test_results = [False, True, True, False]
+# for i in range(len(test_data)):
+#     yr = test_data[i]
+#     print(yr,"->",end="")
+#     result = is_year_leap(yr)
+#     if result == test_results[i]:
+#         print("OK")
+#     else:
+#         print("Failed")
+
+def is_year_leap(year):
+   if year % 4 != 0:
+        return False
+   elif year % 100 != 0:
+        return True
+   elif year % 400 != 0:
+        return False
+   else:
+        return True
+
+def days_in_month(year, month):
+    if month == 2:
+        if is_year_leap(year):
+            return 29
+        else:
+            return 28
+    elif month in [1, 3, 5, 7, 8, 6, 9, 10, 12]:
+        return 31
+   
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+    yr = test_years[i]
+    mo = test_months[i]
+    print(yr, mo, "->", end="")
+    result = days_in_month(yr, mo)
     if result == test_results[i]:
         print("OK")
     else:
