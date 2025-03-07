facfa01ccf9ade356d754e9928868484ada8914b
Odallo <eugeneodali181@gmail.com>
2025-03-07 11:17:25 +0300

    Auto-commit

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-array_id = np.array([1, 2, 3, 4, 5], [6, 7, 8, 9, 10])
+array_id = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 print(array_id)
 print(type(array_id))
+
+array_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print("Size:", array_3d.size) 
+print("Shape: ", array_3d.shape)
+
 
