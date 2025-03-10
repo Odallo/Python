b0934fd043f5af8f70a282a6e32a7c7068ee1a3b
Odallo <eugeneodali181@gmail.com>
2025-03-10 17:51:16 +0300

    python commits

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,12 +1,19 @@
+# import numpy as np
+
+# array_id = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+# print(array_id)
+# print(type(array_id))
+
+# array_3d = np.array([[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], [[7.0, 8.0, 9.0], [10.0, 11.0, 12.0]]])
+# print("Size:", array_3d.size) 
+# print("Shape: ", array_3d.shape)
+# print("Data Type: ", array_3d.dtype)
+
 import numpy as np
+spaceship = np.array([2000, 3000, 5000, 7000])
 
-array_id = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-print(array_id)
-print(type(array_id))
-
-array_3d = np.array([[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], [[7.0, 8.0, 9.0], [10.0, 11.0, 12.0]]])
-print("Size:", array_3d.size) 
-print("Shape: ", array_3d.shape)
-print("Data Type: ", array_3d.dtype)
-
-
+print("Spaceship: ", spaceship)
+print("Size: ", spaceship.size)
+print("Shape: ", spaceship.shape)
+print("Data Type: ", spaceship.dtype)
+print ("Item size: ", spaceship.itemsize)
